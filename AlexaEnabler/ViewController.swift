85b64d9debaa8cdd4861c9339c2b513bdcba1bea
Kumar, Sunil <Sunil.Kumar2@Honeywell.com>
2018-05-26 21:46:29 +0530

    Handled Keyboard Open

--- AlexaEnabler/ViewController.swift
+++ AlexaEnabler/ViewController.swift
@@ -10,10 +10,16 @@ import UIKit
 import SwiftWebVC
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var keyboardHeightLayoutConstraint: NSLayoutConstraint?
+    @IBOutlet weak var textFieldString: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(self.keyboardNotification(notification:)),
+                                               name: NSNotification.Name.UIKeyboardWillChangeFrame,
+                                               object: nil)
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,23 +28,50 @@ class ViewController: UIViewController {
     }
 
     @IBAction func enableButtonPressed(_ sender: UIButton) {
-        let customURL = URL(string: "alexa://alexa?fragment=skills/dp/B01N1ZVI7M")!
         
-        if UIApplication.shared.canOpenURL(customURL) {
-            if #available(iOS 10.0, *) {
-                UIApplication.shared.open(customURL)
+        if let skillID = textFieldString.text as String? {
+            let deeplinkingString = "alexa://alexa?fragment=skills/dp/\(skillID)"
+            let customURL = URL(string: deeplinkingString)!
+            
+            if UIApplication.shared.canOpenURL(customURL) {
+                if #available(iOS 10.0, *) {
+                    UIApplication.shared.open(customURL)
+                } else {
+                    UIApplication.shared.openURL(customURL)
+                }
             } else {
-                UIApplication.shared.openURL(customURL)
+                self.openEmbeddedWebUrl(skillId: skillID)
             }
-        } else {
-            self.openEmbeddedWebUrl()
         }
+
     }
     
-    func openEmbeddedWebUrl() {
-        let webVC = SwiftModalWebVC(urlString: "https://www.amazon.com/dp/B01N1ZVI7M", theme: .lightBlue, dismissButtonStyle: .cross)
+    func openEmbeddedWebUrl(skillId: String) {
+        let webVC = SwiftModalWebVC(urlString: "https://www.amazon.com/dp/\(skillId)", theme: .lightBlue, dismissButtonStyle: .cross)
         webVC.isToolbarHidden = true
         self.present(webVC, animated: true, completion: nil)
+    }
+    
+    
+    @objc func keyboardNotification(notification: NSNotification) {
+        if let userInfo = notification.userInfo {
+            let endFrame = (userInfo[UIKeyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue
+            let endFrameY = endFrame?.origin.y ?? 0
+            let duration:TimeInterval = (userInfo[UIKeyboardAnimationDurationUserInfoKey] as? NSNumber)?.doubleValue ?? 0
+            let animationCurveRawNSN = userInfo[UIKeyboardAnimationCurveUserInfoKey] as? NSNumber
+            let animationCurveRaw = animationCurveRawNSN?.uintValue ?? UIViewAnimationOptions.curveEaseInOut.rawValue
+            let animationCurve:UIViewAnimationOptions = UIViewAnimationOptions(rawValue: animationCurveRaw)
+            if endFrameY >= UIScreen.main.bounds.size.height {
+                self.keyboardHeightLayoutConstraint?.constant = 0.0
+            } else {
+                self.keyboardHeightLayoutConstraint?.constant = 0 - (endFrame?.size.height)!
+            }
+            UIView.animate(withDuration: duration,
+                           delay: TimeInterval(0),
+                           options: animationCurve,
+                           animations: { self.view.layoutIfNeeded() },
+                           completion: nil)
+        }
     }
     
 }
